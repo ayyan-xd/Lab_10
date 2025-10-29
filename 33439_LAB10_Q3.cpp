019d1874009ae19d0afa2d92361d638de17f5310
ayyan-xd <ayyanibrahim44@gmail.com>
2025-10-29 10:40:02 +0500

    Enhance matrix addition program with display
    Added matrix display and sum functionality.

--- 33439_LAB10_Q3.cpp
+++ 33439_LAB10_Q3.cpp
@@ -6,6 +6,23 @@ return x+y;
 int main(){
 int a[3][4] = {{2,5,7,1}, {1,3,6,1}, {5,4,1,3}};
 int b[3][4] = {{1,9,5,0}, {7,1,5,4}, {3,4,2,8}};
+    cout<<"Matrix 1 : "<<endl;
+    for(int i=0 ; i<3 ; i++){
+    for(int j=0 ; j<4 ; j++){
+        cout<<a[i][j]<<"\t";
+    }
+    cout<<endl;
+}
+    cout<<endl;
+    cout<<"Matrix 2 : "<<endl;
+    for(int i=0 ; i<3 ; i++){
+    for(int j=0 ; j<4 ; j++){
+        cout<<b[i][j]<<"\t";
+    }
+    cout<<endl;
+}
+cout<<endl;
+cout<<"Sum Of Matrices is : "<<endl;
 for(int i=0; i<3; i++){
     for(int j=0 ; j<4; j++){
         cout<<mat_add(a[i][j], b[i][j])<<"\t";
