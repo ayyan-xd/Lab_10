a6fa9aa39ff419ccc4eb78961e82d3170be3dcd1
ayyan-xd <ayyanibrahim44@gmail.com>
2025-10-29 10:15:09 +0500

    Add files via upload

---
+++ 33439_LAB10_Q7.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void chain(int size , int arr[]){
+int count=1, num=0, tempcount=1;
+    for(int i=0 ; i<size-1 ;i++){
+    if(arr[i]==arr[i+1]){
+    tempcount++;
+    }
+    else tempcount=1;
+    if(tempcount>count){
+        num=arr[i];
+        count=tempcount;
+    }       
+}
+    cout<<"Longest run : "<<count<<" Consecutive "<<num<<"s";
+}
+int main(){
+    int size;
+    cout<<"Enter number of values : ";
+    cin>>size;
+    int arr[100];
+    cout<<"Enter values : ";
+    for(int i=0 ; i<size  ; i++){
+        cin>>arr[i];
+    }
+    chain(size, arr);
+    return 0;
+}
